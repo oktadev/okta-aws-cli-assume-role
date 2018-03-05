be143ade9a6653798b3df84bc0ea1480a41f2ed8
Alain O'Dea <AlainODea@users.noreply.github.com>
2018-03-05 16:09:41 -0330

    :seedling: Install-friendly config and scripts (#92)
    * :seedling: Allow config.properties in ~/.okta/
    
     - Allow use with config.properties in current working directory
    
     - Allow use with just environment variables and no config.properties
    
    * :seedling: Install-ready scripts and doc
    
     - Install JAR to ~/.okta
    
     - Install scripts anywhere on **PATH**
    
     - Retain old **out** scripts to avoid breaking existing uses

--- src/main/java/com/okta/tools/OktaAwsConfig.java
+++ src/main/java/com/okta/tools/OktaAwsConfig.java
@@ -1,22 +1,25 @@
 package com.okta.tools;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
 import java.util.Properties;
 
 final class OktaAwsConfig {
-    static OktaAwsCliAssumeRole createAwscli() throws IOException {
+
+    private static final String CONFIG_FILENAME = "config.properties";
+
+    static OktaAwsCliAssumeRole createAwscli() {
         Properties properties = new Properties();
-        InputStream config;
-        try {
-            config = new FileInputStream("config.properties");
-        } catch (IOException ex) {
-            // Fallback to classpath input stream
-            config = properties.getClass().getResourceAsStream("/config.properties");
-        }
-        properties.load(new InputStreamReader(config));
+        getConfigFile().ifPresent(configFile -> {
+            try (InputStream config = new FileInputStream(configFile.toFile())) {
+                properties.load(new InputStreamReader(config));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
 
         return OktaAwsCliAssumeRole.createOktaAwsCliAssumeRole(
                 getEnvOrConfig(properties, "OKTA_ORG"),
@@ -27,6 +30,21 @@ final class OktaAwsConfig {
                 getEnvOrConfig(properties, "OKTA_AWS_ROLE_TO_ASSUME")
 
         );
+    }
+
+    private static Optional<Path> getConfigFile()
+    {
+        Path configInWorkingDir = Paths.get(CONFIG_FILENAME);
+        if (Files.isRegularFile(configInWorkingDir)) {
+            return Optional.of(configInWorkingDir);
+        }
+        Path userHome = Paths.get(System.getProperty("user.home"));
+        Path oktaDir = userHome.resolve(".okta");
+        Path configInOktaDir = oktaDir.resolve(CONFIG_FILENAME);
+        if (Files.isRegularFile(configInOktaDir)) {
+            return Optional.of(configInOktaDir);
+        }
+        return Optional.empty();
     }
 
     private static String getEnvOrConfig(Properties properties, String propertyName) {
