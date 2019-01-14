b491e07c53199ef19bffc5f6299e44e669daad5b
Alan Parker Lue <alan.parkerlue@gmail.com>
2019-01-14 11:38:19 -0500

    Here documents and minor cleanup for install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -16,71 +16,70 @@
 #
 PREFIX=${PREFIX:=/usr/local}
 
-java -version > /dev/null 2>&1
-if [ $? -ne 0 ];
-then
-    echo 'Warning: Java is not installed. Make sure to install that'
+dotokta=${HOME}/.okta
+repo_url="https://github.com/oktadeveloper/okta-aws-cli-assume-role"
+
+if ! java -version &>/dev/null; then
+    echo "Warning: Java is not installed. Make sure to install that" >&2
 fi
-aws --version > /dev/null 2>&1
-if [ $? -ne 0 ];
-then
-    echo 'Warning: AWS CLI is not installed. Make sure to install that'
+if ! aws --version &>/dev/null; then
+    echo "Warning: AWS CLI is not installed. Make sure to install that" >&2
 fi
 
-mkdir -p ${HOME}/.okta
-releaseUrl=$(curl --head --silent https://github.com/oktadeveloper/okta-aws-cli-assume-role/releases/latest | grep 'Location:' | cut -c11-)
+mkdir -p ${dotokta}
+releaseUrl=$(curl --head --silent ${repo_url}/releases/latest | grep "Location:" | cut -c11-)
 releaseTag=$(echo $releaseUrl | awk 'BEGIN{FS="/"}{print $8}' | tr -d '\r')
-curl -L "https://github.com/oktadeveloper/okta-aws-cli-assume-role/releases/download/${releaseTag}/okta-aws-cli-${releaseTag:1}.jar" --output "${HOME}/.okta/okta-aws-cli.jar"
+curl -L "${repo_url}/releases/download/${releaseTag}/okta-aws-cli-${releaseTag:1}.jar" \
+     --output "${dotokta}/okta-aws-cli.jar"
 
 # bash functions
-bash_functions="${HOME}/.okta/bash_functions"
-grep '^#OktaAWSCLI' "${bash_functions}" > /dev/null 2>&1
-if [ $? -ne 0 ]
-then
-echo '
+bash_functions="${dotokta}/bash_functions"
+if ! grep '^#OktaAWSCLI' "${bash_functions}" &>/dev/null; then
+    cat <<'EOF' >>"${bash_functions}"
 function okta-aws {
     withokta "aws --profile $1" $@
 }
 function okta-sls {
     withokta "sls --stage $1" $@
 }
-' >> "${bash_functions}"
+EOF
 fi
 
 # Create fish shell functions
 fishFunctionsDir="${HOME}/.config/fish/functions"
 mkdir -p "${fishFunctionsDir}"
-echo '
+cat <<'EOF' >"${fishFunctionsDir}/okta-aws.fish"
 function okta-aws
     withokta "aws --profile $argv[1]" $argv
 end
-' > "${fishFunctionsDir}/okta-aws.fish"
-echo '
+EOF
+cat <<'EOF' >"${fishFunctionsDir}/okta-sls.fish"
 function okta-sls
     withokta "sls --stage $argv[1]" $argv
 end
-' >> "${fishFunctionsDir}/okta-sls.fish"
+EOF
 
 # Conditionally update bash profile
 bashProfile="${HOME}/.bash_profile"
-grep '^#OktaAWSCLI' "${bashProfile}" > /dev/null 2>&1
-if [ $? -ne 0 ]
-then
-echo "
+if ! grep '^#OktaAWSCLI' "${bashProfile}" &>/dev/null; then
+    cat <<EOF >>"${bashProfile}"
 #OktaAWSCLI
-if [ -f \"${bash_functions}\" ]; then
-    . \"${bash_functions}\"
+if [ -f "${bash_functions}" ]; then
+    . "${bash_functions}"
 fi
-" >> "${bashProfile}"
+EOF
 fi
 
-# Suppress "Your profile name includes a 'profile ' prefix" warnings from AWS Java SDK (Resolves #233)
-loggingProperties="${HOME}/.okta/logging.properties"
-echo "com.amazonaws.auth.profile.internal.BasicProfileConfigLoader = NONE
-" > "${loggingProperties}"
+# Suppress "Your profile name includes a 'profile ' prefix" warnings
+# from AWS Java SDK (Resolves #233)
+loggingProperties="${dotokta}/logging.properties"
+cat <<EOF >"${loggingProperties}"
+com.amazonaws.auth.profile.internal.BasicProfileConfigLoader = NONE
+EOF
 
 # Create withokta command
-echo '#!/bin/bash
+cat <<'EOF' >"${PREFIX}/bin/withokta"
+#!/bin/bash
 command="$1"
 profile=$2
 shift;
@@ -89,34 +88,34 @@ env OKTA_PROFILE=$profile java \
     -Djava.util.logging.config.file=~/.okta/logging.properties \
     -classpath ~/.okta/okta-aws-cli.jar \
     com.okta.tools.WithOkta $command $@
-' > "$PREFIX/bin/withokta"
-chmod +x "$PREFIX/bin/withokta"
+EOF
+chmod +x "${PREFIX}/bin/withokta"
 
 # Create okta-credential_process command
-echo '#!/bin/bash
+cat <<'EOF' >"${PREFIX}/bin/okta-credential_process"
+#!/bin/bash
 roleARN="$1"
 shift;
 env OKTA_AWS_ROLE_TO_ASSUME="$roleARN" \
     java -classpath ~/.okta/okta-aws-cli.jar com.okta.tools.CredentialProcess
-' > "$PREFIX/bin/okta-credential_process"
-chmod +x "$PREFIX/bin/okta-credential_process"
+EOF
+chmod +x "${PREFIX}/bin/okta-credential_process"
 
 # Create okta-listroles command
-echo '#!/bin/bash
+cat <<EOF >"${PREFIX}/bin/okta-listroles"
+#!/bin/bash
 java -classpath ~/.okta/okta-aws-cli.jar com.okta.tools.ListRoles
-' > "$PREFIX/bin/okta-listroles"
-chmod +x "$PREFIX/bin/okta-listroles"
+EOF
+chmod +x "${PREFIX}/bin/okta-listroles"
 
 # Configure Okta AWS CLI
-oktaConfig="${HOME}/.okta/config.properties"
-grep '^#OktaAWSCLI' "${oktaConfig}" > /dev/null 2>&1
-if [ $? -ne 0 ]
-then
-echo "
+oktaConfig="${dotokta}/config.properties"
+if ! grep '^#OktaAWSCLI' "${oktaConfig}" &>/dev/null; then
+    cat <<EOF >"${oktaConfig}"
 #OktaAWSCLI
 OKTA_ORG=acmecorp.okta.com.changeme.local
 OKTA_AWS_APP_URL=https://acmecorp.oktapreview.com.changeme.local/home/amazon_aws/0oa5zrwfs815KJmVF0h7/137
 OKTA_USERNAME=$env:USERNAME
 OKTA_BROWSER_AUTH=true
-" > "${oktaConfig}"
+EOF
 fi
