f082b5b3b149c23b06f138020aace8dbc687f0a2
Sam Mingo <github@jake8us.org>
2021-06-07 13:27:55 -0400

    Update install.sh (#342)
    Adds logging.properties to all `/bin` scripts

--- bin/install.sh
+++ bin/install.sh
@@ -160,8 +160,11 @@ if [ -n "\$https_proxy" ]; then
     readonly URI_REGEX='^(([^:/?#]+):)?(//((([^:/?#]+)@)?([^:/?#]+)(:([0-9]+))?))?(/([^?#]*))(\?([^#]*))?(#(.*))?'
     [[ \$https_proxy =~ \${URI_REGEX} ]] && PROXY_CONFIG="-Dhttps.proxyHost=\${BASH_REMATCH[7]} -Dhttps.proxyPort=\${BASH_REMATCH[9]}"
 fi
-env OKTA_AWS_ROLE_TO_ASSUME="\$roleARN" \\
-    java \${PROXY_CONFIG} -classpath ${PREFIX}/okta-aws-cli.jar com.okta.tools.CredentialProcess
+env OKTA_AWS_ROLE_TO_ASSUME="\$roleARN" \
+    java \${PROXY_CONFIG} \
+      -Djava.util.logging.config.file=${PREFIX}/logging.properties \
+      -classpath ${PREFIX}/okta-aws-cli.jar \
+      com.okta.tools.CredentialProcess
 EOF
 chmod +x "${PREFIX}/bin/okta-credential_process"
 
@@ -172,7 +175,10 @@ if [ -n "\$https_proxy" ]; then
     readonly URI_REGEX='^(([^:/?#]+):)?(//((([^:/?#]+)@)?([^:/?#]+)(:([0-9]+))?))?(/([^?#]*))(\?([^#]*))?(#(.*))?'
     [[ \$https_proxy =~ \${URI_REGEX} ]] && PROXY_CONFIG="-Dhttps.proxyHost=\${BASH_REMATCH[7]} -Dhttps.proxyPort=\${BASH_REMATCH[9]}"
 fi
-java \${PROXY_CONFIG} -classpath ${PREFIX}/okta-aws-cli.jar com.okta.tools.ListRoles
+java \${PROXY_CONFIG} \
+  -Djava.util.logging.config.file=${PREFIX}/logging.properties \
+  -classpath ${PREFIX}/okta-aws-cli.jar \
+  com.okta.tools.ListRoles
 EOF
 chmod +x "${PREFIX}/bin/okta-listroles"
 
