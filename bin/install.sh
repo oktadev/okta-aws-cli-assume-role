03b281907e7dd67eb2efa7d429372c79271d4681
Russ Sivak <Russ.Sivak@moodys.com>
2021-08-04 15:16:53 -0400

    make grep for the location tag case insensitive

--- bin/install.sh
+++ bin/install.sh
@@ -80,7 +80,7 @@ PREFIX="$(cd -P -- "${PREFIX}" && pwd)"
 echo "Installing into ${PREFIX}" | sed "s#$HOME#~#g"
 
 mkdir -p ${PREFIX}
-releaseUrl=$(curl -sLI ${repo_url}/releases/latest | grep -e "location:.*tag" | cut -c11-)
+releaseUrl=$(curl -sLI ${repo_url}/releases/latest | grep -ie "location:.*tag" | cut -c11-)
 releaseTag=$(echo $releaseUrl | awk 'BEGIN{FS="/"}{print $8}' | tr -d '\r')
 url=${repo_url}/releases/download/${releaseTag}/okta-aws-cli-${releaseTag:1}.jar
 dest=${PREFIX}/$(basename ${url})
